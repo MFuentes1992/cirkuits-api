64cf56eca2c43756ff607ac0d6c0195c1ffd6922
Marco Fuentes <marco.fuentes.teseracto@smartbusinesscorp.com>
2024-02-13 10:17:13 -0600

    Modify email template

--- src/main/java/com/cirkuits/cirkuitsapi/EmailService/EmailService.java
+++ src/main/java/com/cirkuits/cirkuitsapi/EmailService/EmailService.java
@@ -2,10 +2,11 @@ package com.cirkuits.cirkuitsapi.EmailService;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeBodyPart;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-
+import java.util.UUID;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -23,26 +24,25 @@ public class EmailService {
     }
 
     public void sendEmail(String sender, String emailDest, String subject, String templateName) throws MessagingException {
-
+        String cid = UUID.randomUUID().toString();
         MimeMessage message = mailSender.createMimeMessage();
         message.setFrom(new InternetAddress(sender));
         message.setRecipients(MimeMessage.RecipientType.TO, emailDest);
         message.setSubject(subject);
         String htmlTemplate = "";
         try {
-            // Read the HTML template into a String variable
              htmlTemplate = readFile("src/main/resources/templates/"+templateName+".html");
+            // -- Attach image
+            MimeBodyPart imagePart = new MimeBodyPart();
+            imagePart.attachFile("resources/teapot.jpg");
+            imagePart.setContentID("<" + cid + ">");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        // Replace placeholders in the HTML template with dynamic values
-        // htmlTemplate = htmlTemplate.replace("${name}", "John Doe");
-        // htmlTemplate = htmlTemplate.replace("${message}", "Hello, this is a test email.");
+        String cleanTemplate = htmlTemplate.replace("{cid}", cid);
+        message.setContent(cleanTemplate, "text/html; charset=utf-8");
 
-        // Set the email's content to be the HTML template
-        System.out.println(htmlTemplate);
-        message.setContent(htmlTemplate, "text/html; charset=utf-8");
-        if(htmlTemplate.isEmpty())
+        if(cleanTemplate.isEmpty())
             throw new MessagingException("Template cannot be null");
         mailSender.send(message);
     }
