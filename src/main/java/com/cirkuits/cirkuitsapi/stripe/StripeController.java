959c24535e428269a3cf3c4f046744358ad05511
Marco Fuentes <marco.fuentes.teseracto@smartbusinesscorp.com>
2024-04-21 22:30:20 -0600

    Include secret key ENV variable

--- src/main/java/com/cirkuits/cirkuitsapi/stripe/StripeController.java
+++ src/main/java/com/cirkuits/cirkuitsapi/stripe/StripeController.java
@@ -5,6 +5,7 @@ import com.cirkuits.cirkuitsapi.stripe.service.StripeService;
 import com.cirkuits.cirkuitsapi.user.UserService;
 import com.cirkuits.cirkuitsapi.user.Users;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,10 +17,11 @@ import com.stripe.Stripe;
 public class StripeController {
     @Autowired
     UserService userService;
-
+    @Value("${cirkuits.stripe.secret.key}")
+    private String secretKey;
     @PostMapping (path = "api/v1/stripe/payment-sheet")
     public ResponseEntity<Object> paymentSheet(@RequestBody PurchaseIntent pIntent) throws Exception {
-        Stripe.apiKey = "sk_test_51HTgMwHZLa5RkL3dWhGh7hXaAPV61rP2GdxT2xigI4sMEOR94B2DNYmMY30LCpOgCtC7l5gEmvVUeyYqreqnRftA00p1FraSUn";
+        Stripe.apiKey = secretKey;
         // -- If customer Id does not exist in DB, create a new customer
         // -- Customer must have full name, email, and phone number
         Users user = userService.getUserEmail(pIntent.getEmail());
